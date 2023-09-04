967357e1ae6d17c5f7b5132d337a410532bc3ff6
Phylargia <135708864+Phylargia@users.noreply.github.com>
2023-09-04 12:31:53 +0100

    Update run.sh

--- exoseq_hawk/run.sh
+++ exoseq_hawk/run.sh
@@ -9,6 +9,6 @@
 #SBATCH --account=scw1557 # Change Project group
 
 module load nextflow/21.10.6
-cd /scratch/c.c1845715/nextflow_build/exoseq_hawk # Change User ID
+cd /scratch/c.c1845715/nextflow_cellranger/exoseq_hawk # Change User ID
 
 nextflow run main.nf --genome mouse --input 'input/input.csv' -with-trace
