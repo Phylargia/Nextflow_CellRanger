4c72ea79209c871fca4ab3f9861d5726ae1ecf65
Phylargia <135708864+Phylargia@users.noreply.github.com>
2023-09-19 12:25:32 +0100

    Create DEG.py

---
+++ Py_scripts/DEG.py
@@ -0,0 +1,13 @@
+# Setup
+# Import modules
+import os
+import sys
+import time 
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import matplotlib.pyplot as plt
+import scvi
+
+import seaborn as sns
+from scipy.stats import median_abs_deviation
